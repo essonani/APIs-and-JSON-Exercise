d460f03203cfc26ea72f43215380051551c835c5
Essonani Tchakpalla <130189659+essonani@users.noreply.github.com>
2024-03-12 20:44:06 -0500

    Add files via upload

---
+++ APIs-and-JSON-Exercise/APIsAndJSON/FamousQuote.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace APIsAndJSON
+{
+    public class FamousQuote
+    {
+        public static void Kanyequote()
+        {
+            var client = new HttpClient();
+            var kanyeUrl = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeUrl).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine("kanye :" +kanyeQuote);
+        }
+       public static void ronQuote()
+        {
+            var client = new HttpClient();
+            var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse= client.GetStringAsync(ronUrl).Result;
+            var ronQuote= JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine("ron: "+ronQuote);
+        }
+    }
+}
---
+++ APIs-and-JSON-Exercise/APIsAndJSON/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using Newtonsoft.Json.Linq;
+namespace APIsAndJSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FamousQuote.Kanyequote();
+            FamousQuote.ronQuote();
+        }
+    }
+}
